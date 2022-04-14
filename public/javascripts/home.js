71e2e699c212812b85efd10bb1a3d5dc408ceea9
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2022-04-14 13:10:18 +0530

    Homepage

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,17 +1,13 @@
-// javascript for Toggle Menu
-var navlink = document.getElementById("nav-links");
-var box = document.getElementById("box");
-var home = document.getElementById("Home");
-const ShowMenu = () => {
-    navlink.style.width = '100%';
-    box.style.left = '0%';
-    document.querySelector('body').style.overflow = 'hidden';
-}
-const HideMenu = () => {
-    navlink.style.width = '0%';
-    box.style.left = '-20%';
-    document.querySelector('body').style.overflow = 'scroll';
-}
-const backHome = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
