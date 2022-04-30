837f0c1828bcf3b8a276bc43657356975ac6e871
Lohith <srklohith05@gmail.com>
2022-04-30 17:14:03 +0530

    changes

--- routes/training.js
+++ routes/training.js
@@ -1,128 +1,128 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const db = require("../database");
-const multer = require("multer");
-const path = require("path");
-const {storage, cloudinary} = require("../cloudinary");
-const upload = multer({storage});
-const {isloggedin} = require("../middleware");
+const db = require('../database');
+const multer = require('multer');
+const path = require('path');
+const { storage, cloudinary } = require('../cloudinary');
+const upload = multer({ storage });
+const { isloggedin } = require('../middleware');
 
 const router = express.Router();
 
-router.get("/training", (req, res) => {
-    db.query("SELECT * FROM training", (err, rows) => {
-        if (!err) {
-            res.render("training", {training: rows});
-        } else {
-            res.status(500).send("Internal server error");
-            console.log(err);
-        }
-    });
+router.get('/training', (req, res) => {
+	db.query('SELECT * FROM training', (err, rows) => {
+		if (!err) {
+			res.render('training', { training: rows });
+		} else {
+			res.status(500).send('Internal server error');
+			console.log(err);
+		}
+	});
 });
 
-router.get("/admin/training", isloggedin, (req, res) => {
-    db.query("SELECT * FROM training", (err, rows) => {
-        if (!err) {
-            res.render("./admin/training/training_show", {trainingArray: rows});
-        } else {
-            res.status(500).send("Internal server error");
-            console.log(err);
-        }
-    });
+router.get('/admin/training', isloggedin, (req, res) => {
+	db.query('SELECT * FROM training', (err, rows) => {
+		if (!err) {
+			res.render('./admin/training/training_show', { trainingArray: rows });
+		} else {
+			res.status(500).send('Internal server error');
+			console.log(err);
+		}
+	});
 });
 
-router.get("/admin/training/insert", isloggedin, async (req, res) => {
-    db.query("SELECT * FROM training", async (err, rows) => {
-        if (!err) {
-            res.render("./admin/training/training_insert");
-        } else {
-            console.log(err);
-        }
-    });
+router.get('/admin/training/insert', isloggedin, async (req, res) => {
+	db.query('SELECT * FROM training', async (err, rows) => {
+		if (!err) {
+			res.render('./admin/training/training_insert');
+		} else {
+			console.log(err);
+		}
+	});
 });
 
-router.post("/admin/training/insert", isloggedin, async (req, res) => {
-    db.query(
-        `INSERT INTO training (title_training, paragraph_training, batch_training) VALUES (?, ?, ?)`,
-        [
-            req.body.title_training,
-            req.body.paragraph_training,
-            req.body.batch_training,
-        ],
-        async (err, rows) => {
-            if (!err) {
-                res.redirect("/admin/training");
-            } else {
-                console.log(err);
-            }
-        }
-    );
+router.post('/admin/training/insert', isloggedin, async (req, res) => {
+	db.query(
+		`INSERT INTO training (title_training, paragraph_training, batch_training) VALUES (?, ?, ?)`,
+		[
+			req.body.title_training,
+			req.body.paragraph_training,
+			req.body.batch_training
+		],
+		async (err, rows) => {
+			if (!err) {
+				res.json('/admin/training');
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
-router.get("/admin/training/:id", isloggedin, (req, res) => {
-    db.query(
-        "SELECT * FROM training WHERE id_training = ?",
-        [req.params.id],
-        (err, rows) => {
-            if (!err) {
-                res.render("./admin/training/training_view", {
-                    training: rows[0],
-                });
-            } else {
-                res.status(500).send("Internal server error");
-                console.log(err);
-            }
-        }
-    );
+router.get('/admin/training/:id', isloggedin, (req, res) => {
+	db.query(
+		'SELECT * FROM training WHERE id_training = ?',
+		[req.params.id],
+		(err, rows) => {
+			if (!err) {
+				res.render('./admin/training/training_view', {
+					training: rows[0]
+				});
+			} else {
+				res.status(500).send('Internal server error');
+				console.log(err);
+			}
+		}
+	);
 });
 
-router.get("/admin/training/update/:id", isloggedin, async (req, res) => {
-    db.query(
-        `SELECT * FROM training WHERE id_training = ?`,
-        [req.params.id],
-        async (err, rows) => {
-            if (!err) {
-                res.render("./admin/training/training_update", {
-                    training: rows[0],
-                });
-            } else {
-                console.log(err);
-            }
-        }
-    );
+router.get('/admin/training/update/:id', isloggedin, async (req, res) => {
+	db.query(
+		`SELECT * FROM training WHERE id_training = ?`,
+		[req.params.id],
+		async (err, rows) => {
+			if (!err) {
+				res.render('./admin/training/training_update', {
+					training: rows[0]
+				});
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
-router.post("/admin/training/update/:id", isloggedin, async (req, res) => {
-    db.query(
-        `UPDATE training SET title_training = ?, paragraph_training = ?, batch_training = ?  WHERE id_training = ?`,
-        [
-            req.body.title_training,
-            req.body.paragraph_training,
-            req.body.batch_training,
-            req.params.id,
-        ],
-        async (err, rows) => {
-            if (!err) {
-                res.redirect("/admin/training");
-            } else {
-                console.log(err);
-            }
-        }
-    );
+router.post('/admin/training/update/:id', isloggedin, async (req, res) => {
+	db.query(
+		`UPDATE training SET title_training = ?, paragraph_training = ?, batch_training = ?  WHERE id_training = ?`,
+		[
+			req.body.title_training,
+			req.body.paragraph_training,
+			req.body.batch_training,
+			req.params.id
+		],
+		async (err, rows) => {
+			if (!err) {
+				res.redirect('/admin/training');
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
-router.get("/admin/training/delete/:id", isloggedin, async (req, res) => {
-    db.query(
-        `DELETE FROM training WHERE id_training = ?`,
-        [req.params.id],
-        async (err, rows) => {
-            if (!err) {
-                res.redirect("/admin/training");
-            } else {
-                console.log(err);
-            }
-        }
-    );
+router.get('/admin/training/delete/:id', isloggedin, async (req, res) => {
+	db.query(
+		`DELETE FROM training WHERE id_training = ?`,
+		[req.params.id],
+		async (err, rows) => {
+			if (!err) {
+				res.redirect('/admin/training');
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
 module.exports = router;
