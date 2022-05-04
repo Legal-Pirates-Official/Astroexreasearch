43fe967522a66921345d15801cf977e9cd855cce
Prasanna <prasannavelmurugan0200@gmail.com>
2022-05-04 10:14:57 +0530

    rv

--- routes/simple.js
+++ routes/simple.js
@@ -4,9 +4,7 @@ const db = require("../database");
 
 const router = express.Router();
 
-// router.get("/", (req, res) => {
-//     res.render("index");
-// });
+
 
 router.get("/about", (req, res) => {
     db.query("SELECT * FROM partners", (err, result) => {
@@ -29,27 +27,11 @@ router.get("/about", (req, res) => {
     });
 });
 
-// router.get("/events", (req, res) => {
-//     res.render("events");
-// });
-
-// router.get("/contact", (req, res) => {
-//     res.render("contact");
-// });
 
 router.get("/partners", (req, res) => {
     res.render("partners");
 });
 
-// router.get("/teams", (req, res) => {
-//     res.render("teams");
-// });
-
-// router.get("/research_areas", (req, res) => {
-//     res.render("research_areas");
-// });
-// router.get("/training", (req, res) => {
-//     res.render("training");
-// });
+;
 
 module.exports = router;
