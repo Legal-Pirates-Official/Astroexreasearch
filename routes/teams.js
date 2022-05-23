0c3511eed5b3eeaf2c1b693299a49326047c0f4a
Lohith <srklohith05@gmail.com>
2022-05-23 21:50:43 +0530

    changes

--- routes/teams.js
+++ routes/teams.js
@@ -1,151 +1,160 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const db = require("../database");
-const multer = require("multer");
-const path = require("path");
-const {storage, cloudinary} = require("../cloudinary");
-const upload = multer({storage});
-const {isloggedin} = require("../middleware");
+const db = require('../database');
+const multer = require('multer');
+const path = require('path');
+const { storage, cloudinary } = require('../cloudinary');
+const upload = multer({ storage });
+const { isloggedin } = require('../middleware');
 
 const router = express.Router();
 
-router.get("/teams", (req, res) => {
-    db.query("SELECT * FROM teams", (err, rows) => {
-        if (!err) {
-            res.render("teams", {teamArray: rows});
-        } else {
-            res.status(500).send("Internal server error");
-            console.log(err);
-        }
-    });
+router.get('/teams', (req, res) => {
+	db.query('SELECT * FROM teams', (err, rows) => {
+		if (!err) {
+			res.render('teams', {
+				teamArray: rows
+			});
+		} else {
+			res.status(500).send('Internal server error');
+			console.log(err);
+		}
+	});
 });
 
-router.get("/admin/teams", isloggedin, (req, res) => {
-    db.query("SELECT * FROM teams", (err, rows) => {
-        if (!err) {
-            res.render("./admin/teams/team_show", {teamArray: rows});
-        } else {
-            res.status(500).send("Internal server error");
-            console.log(err);
-        }
-    });
+router.get('/admin/teams', isloggedin, (req, res) => {
+	const message = req.flash('success');
+	db.query('SELECT * FROM teams', (err, rows) => {
+		if (!err) {
+			res.render('./admin/teams/team_show', {
+				teamArray: rows,
+				message
+			});
+		} else {
+			res.status(500).send('Internal server error');
+			console.log(err);
+		}
+	});
 });
 
-router.get("/admin/teams/insert", isloggedin, async (req, res) => {
-    db.query("SELECT * FROM teams", async (err, rows) => {
-        if (!err) {
-            res.render("./admin/teams/team_insert");
-        } else {
-            console.log(err);
-        }
-    });
-});
-
-router.post(
-    "/admin/teams/insert",
-    upload.single("image_team"),
-    isloggedin,
-    async (req, res) => {
-        db.query(
-            `INSERT INTO teams (name_team, job_team, email_team, instagram_url, linkedIn_url,  image_team, select_team ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-            [
-                req.body.name_team,
-                req.body.job_team,
-                req.body.email_team,
-                req.body.instagram_url,
-                req.body.linkedIn_url,
-                req.file.path,
-                req.body.select_team,
-            ],
-            async (err, rows) => {
-                if (!err) {
-                    res.redirect("/admin/teams");
-                } else {
-                    console.log(err);
-                }
-            }
-        );
-    }
-);
-
-router.get("/admin/teams/:id", isloggedin, (req, res) => {
-    db.query(
-        "SELECT * FROM teams WHERE id_team = ?",
-        [req.params.id],
-        (err, rows) => {
-            if (!err) {
-                res.render("./admin/teams/team_view", {team: rows[0]});
-            } else {
-                res.status(500).send("Internal server error");
-                console.log(err);
-            }
-        }
-    );
-});
-
-router.get("/admin/teams/update/:id", isloggedin, async (req, res) => {
-    db.query(
-        `SELECT * FROM teams WHERE id_team = ?`,
-        [req.params.id],
-        async (err, rows) => {
-            if (!err) {
-                res.render("./admin/teams/team_update", {
-                    team: rows[0],
-                });
-            } else {
-                console.log(err);
-            }
-        }
-    );
+router.get('/admin/teams/insert', isloggedin, async (req, res) => {
+	db.query('SELECT * FROM teams', async (err, rows) => {
+		if (!err) {
+			res.render('./admin/teams/team_insert');
+		} else {
+			console.log(err);
+		}
+	});
 });
 
 router.post(
-    "/admin/teams/update/:id",
-    isloggedin,
-    upload.single("image_team"),
-    async (req, res) => {
-        const oldimage = req.body.image_checkbox
-            .split("Astroex_Research_Association/")[1]
-            .slice(0, -4);
-
-        db.query(
-            `UPDATE teams SET name_team = ?, job_team = ?, email_team = ?, instagram_url = ?, linkedIn_url = ?, select_team = ? , image_team = ? WHERE id_team = ?`,
-            [
-                req.body.name_team,
-                req.body.job_team,
-                req.body.email_team,
-                req.body.instagram_url,
-                req.body.linkedIn_url,
-                req.body.select_team,
-                req.file ? req.file.path : req.body.image_checkbox,
-                req.params.id,
-            ],
-            async (err, rows) => {
-                if (!err) {
-                    await cloudinary.uploader.destroy(
-                        "Astroex_Research_Association/" + oldimage
-                    );
-                    res.redirect("/admin/teams");
-                } else {
-                    console.log(err);
-                }
-            }
-        );
-    }
+	'/admin/teams/insert',
+	upload.single('image_team'),
+	isloggedin,
+	async (req, res) => {
+		db.query(
+			`INSERT INTO teams (name_team, job_team, email_team, instagram_url, linkedIn_url,  image_team, select_team ) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+			[
+				req.body.name_team,
+				req.body.job_team,
+				req.body.email_team,
+				req.body.instagram_url,
+				req.body.linkedIn_url,
+				req.file.path,
+				req.body.select_team
+			],
+			async (err, rows) => {
+				if (!err) {
+					await req.flash('success', 'Team added successfully');
+					res.redirect('/admin/teams');
+				} else {
+					console.log(err);
+				}
+			}
+		);
+	}
 );
 
-router.get("/admin/teams/delete/:id", isloggedin, async (req, res) => {
-    db.query(
-        `DELETE FROM teams WHERE id_team = ?`,
-        [req.params.id],
-        async (err, rows) => {
-            if (!err) {
-                res.redirect("/admin/teams");
-            } else {
-                console.log(err);
-            }
-        }
-    );
+router.get('/admin/teams/:id', isloggedin, (req, res) => {
+	db.query(
+		'SELECT * FROM teams WHERE id_team = ?',
+		[req.params.id],
+		(err, rows) => {
+			if (!err) {
+				res.render('./admin/teams/team_view', { team: rows[0] });
+			} else {
+				res.status(500).send('Internal server error');
+				console.log(err);
+			}
+		}
+	);
+});
+
+router.get('/admin/teams/update/:id', isloggedin, async (req, res) => {
+	db.query(
+		`SELECT * FROM teams WHERE id_team = ?`,
+		[req.params.id],
+		async (err, rows) => {
+			if (!err) {
+				res.render('./admin/teams/team_update', {
+					team: rows[0]
+				});
+			} else {
+				console.log(err);
+			}
+		}
+	);
+});
+
+router.post(
+	'/admin/teams/update/:id',
+	isloggedin,
+	upload.single('image_team'),
+	async (req, res) => {
+		const oldimage = req.body.image_checkbox
+			.split('Astroex_Research_Association/')[1]
+			.slice(0, -4);
+
+		db.query(
+			`UPDATE teams SET name_team = ?, job_team = ?, email_team = ?, instagram_url = ?, linkedIn_url = ?, select_team = ? , image_team = ? WHERE id_team = ?`,
+			[
+				req.body.name_team,
+				req.body.job_team,
+				req.body.email_team,
+				req.body.instagram_url,
+				req.body.linkedIn_url,
+				req.body.select_team,
+				req.file ? req.file.path : req.body.image_checkbox,
+				req.params.id
+			],
+			async (err, rows) => {
+				if (!err) {
+					await cloudinary.uploader.destroy(
+						'Astroex_Research_Association/' + oldimage
+					);
+					await req.flash('success', 'Team updated successfully');
+					res.redirect('/admin/teams');
+				} else {
+					console.log(err);
+				}
+			}
+		);
+	}
+);
+
+router.get('/admin/teams/delete/:id', isloggedin, async (req, res) => {
+	db.query(
+		`DELETE FROM teams WHERE id_team = ?`,
+		[req.params.id],
+		async (err, rows) => {
+			if (!err) {
+				await req.flash('success', 'Team deleted successfully');
+				res.redirect('/admin/teams');
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,68 +1,69 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const ejsMate = require("ejs-mate");
-const session = require("express-session");
-const ejs = require("ejs");
-const path = require("path");
-const upload = require("express-fileupload");
-
+const bodyParser = require('body-parser');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const ejs = require('ejs');
+const path = require('path');
+const upload = require('express-fileupload');
+var flash = require('connect-flash');
 // routes
 
-const home = require("./routes/home");
-const login = require("./routes/login");
-const teams = require("./routes/teams");
-const training = require("./routes/training");
-const simple = require("./routes/simple");
-const contact = require("./routes/contact");
-const footer = require("./routes/footer");
-const partners = require("./routes/partners");
-const collaborators = require("./routes/collaborators");
-const projects = require("./routes/projects");
-const events = require("./routes/events");
-const research_areas = require("./routes/research_areas");
-const internship = require("./routes/internship");
-const upload_files = require("./routes/upload_files");
+const home = require('./routes/home');
+const login = require('./routes/login');
+const teams = require('./routes/teams');
+const training = require('./routes/training');
+const simple = require('./routes/simple');
+const contact = require('./routes/contact');
+const footer = require('./routes/footer');
+const partners = require('./routes/partners');
+const collaborators = require('./routes/collaborators');
+const projects = require('./routes/projects');
+const events = require('./routes/events');
+const research_areas = require('./routes/research_areas');
+const internship = require('./routes/internship');
+const upload_files = require('./routes/upload_files');
 
-app.use(express.static("public"));
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(flash());
 
-require("dotenv").config();
+require('dotenv').config();
 
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-    })
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: true,
+		saveUninitialized: true
+	})
 );
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/", simple);
-app.use("/", home);
-app.use("/admin", login);
-app.use("/", teams);
-app.use("/", training);
-app.use("/", contact);
-app.use("/", footer);
-app.use("/", partners);
-app.use("/", collaborators);
-app.use("/", projects);
-app.use("/", events);
-app.use("/", research_areas);
-app.use("/", internship);
-app.use("/", upload_files);
+app.use('/', simple);
+app.use('/', home);
+app.use('/admin', login);
+app.use('/', teams);
+app.use('/', training);
+app.use('/', contact);
+app.use('/', footer);
+app.use('/', partners);
+app.use('/', collaborators);
+app.use('/', projects);
+app.use('/', events);
+app.use('/', research_areas);
+app.use('/', internship);
+app.use('/', upload_files);
 
-app.get("*", (req, res) => {
-    res.render("404");
+app.get('*', (req, res) => {
+	res.render('404');
 });
 
 app.listen(`${process.env.PORT}`, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+	console.log(`Server is running on port ${process.env.PORT}`);
 });
