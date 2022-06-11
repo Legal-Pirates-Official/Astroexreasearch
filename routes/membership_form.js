695b3f05cfd59f42770e3395718be0c6a944f2cd
= <=>
2022-06-11 17:30:14 +0530

    update

--- routes/membership_form.js
+++ routes/membership_form.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
 require("dotenv").config();
+const db = require("../database");
 
 const router = express.Router();
 
@@ -9,41 +10,79 @@ router.get("/becomeamember", (req, res) => {
     res.render("membership-form");
 });
 
-router.post("/becomeamember", (req, res) => {
-    const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-    `;
+router.post("/becomeamember/insert", async (req, res) => {
+    // const output = `
+    // <p>You have a new contact request</p>
+    // <h3>Contact Details</h3>
+    // <ul>
+    //     <li>Name: ${req.body.name}</li>
+    //     <li>Email: ${req.body.email}</li>
+    // </ul>
+    // <h3>Message</h3>
+    // <p>${req.body.message}</p>
+    // `;
 
-    let transporter = nodemailer.createTransport({
-        service: "Gmail",
-        host: "smtp.gmail.com",
-        auth: {
-            user: process.env.MAIL,
-            pass: "Latha13087280$#",
-        },
-    });
+    // let transporter = nodemailer.createTransport({
+    //     service: "Gmail",
+    //     host: "smtp.gmail.com",
+    //     auth: {
+    //         user: process.env.MAIL,
+    //         pass: "Latha13087280$#",
+    //     },
+    // });
 
-    let mailOptions = {
-        from: process.env.MAIL,
-        to: process.env.MAIL,
-        subject: "Message from Astroxresearch",
-        text: "Hello world?",
-        html: output,
-    };
+    // let mailOptions = {
+    //     from: process.env.MAIL,
+    //     to: process.env.MAIL,
+    //     subject: "Message from Astroxresearch",
+    //     text: "Hello world?",
+    //     html: output,
+    // };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         return console.log(error);
+    //     }
+    //     res.render("./contact");
+    // });
+
+    await db.query(
+        "INSERT INTO membership_join SET ?",
+        [req.body],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
         }
-        res.render("./contact");
-    });
+    );
+
+
 });
+
+router.get('/becomeamember/show', async (req, res) => {
+    await db.query('SELECT * FROM membership_join', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('membership-show', {
+                membership_join: result
+            });
+        }
+    });
+
+})
+
+router.get('/becomeamember/delete/:id', async (req, res) => {
+    await db.query('DELETE FROM membership_join WHERE id = ?', [req.params.id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/membership-show');
+        }
+    });
+}
+)
 
 module.exports = router;
