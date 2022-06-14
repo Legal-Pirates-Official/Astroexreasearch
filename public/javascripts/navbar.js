6cff7ec24cdbacd1c34fc66be62500e97a67640f
solomonsanthosh <solosanty2064@gmail.com>
2022-06-14 19:20:47 +0530

    SS4

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,12 +1,24 @@
 const dropdownAtag = document.querySelector(".dropdown-atag");
 const dropdownNavbar = document.querySelector(".dropdown-navbar");
 const navBtn = document.querySelector(".nav-btn");
-const fullNav = document.querySelector('.nav-links-2');
+const dropdown = document.querySelectorAll(".dropdown");
+const dropdownContent = document.querySelector(".dropdown-content");
+const fullNav = document.querySelector(".nav-links-2");
 let dropdownNavbarIsOpen = false;
 
+dropdown.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    document.querySelectorAll(`.mobiledrop`).forEach((dropdownContent) => {
+        dropdownContent.style.display = "none";
+    })
+    console.log("clicked",dropdown.getAttribute('data-attr'));
+   const name = dropdown.getAttribute('data-attr')
+    document.querySelector(`.${name}`).style.display = "flex";
+  })
+});
 dropdownAtag.addEventListener("mouseover", () => {
-    dropdownNavbar.classList.add("show");
-    dropdownNavbarIsOpen = true;
+  dropdownNavbar.classList.add("show");
+  dropdownNavbarIsOpen = true;
 });
 
 // navBtn.addEventListener("click", (event) => {
@@ -14,8 +26,8 @@ dropdownAtag.addEventListener("mouseover", () => {
 // });
 
 dropdownAtag.addEventListener("click", () => {
-    dropdownNavbar.classList.remove("show");
+  dropdownNavbar.classList.remove("show");
 });
 window.addEventListener("click", () => {
-    dropdownNavbar.classList.remove("show");
+  dropdownNavbar.classList.remove("show");
 });
